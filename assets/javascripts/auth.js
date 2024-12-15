ad158e1465dbd8f8d3e6acb6bc9e2daa064d51b7
Võ Thị Ánh Nguyệt_22728801 <147931222+VoThiAnhNguyet22728801@users.noreply.github.com>
2024-12-15 07:20:54 +0700

    ....

--- assets/javascripts/auth.js
+++ assets/javascripts/auth.js
@@ -1,19 +1,28 @@
 function register() {
+    const email = document.getElementById('email').value;
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
     const repassword = document.getElementById('repassword').value;
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(!emailRegex.test(email)){
+        alert("Định dạng email không đúng");
+        return;
+    }
 
     if (password !== repassword) {
         alert("Mật khẩu nhập lại không khớp!");
         return;
     }
     const user = {
+        email: email,
         username: username,
         password: password
     };
     localStorage.setItem('user', JSON.stringify(user));
     alert("Đăng ký thành công!");
-    window.location.href = '/account/login.html';
+    window.location.href = 'account/login.html';
 }
 
 function login() {
